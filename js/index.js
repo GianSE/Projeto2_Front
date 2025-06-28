70c21a890968fd5b8016b8e2f52fde0afe355b38
Gian <gianpedrodev@gmail.com>
2025-06-28 04:04:12 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,11 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Verifica se o usuário está logado
     const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
 
-    // Se estiver logado, redireciona para a página de eco-dicas
+    // Se estiver logado, redireciona para a página de eco-dicas e muda o texto
     if (isLoggedIn) {
         botaoComecar.href = 'ecodicas.html';
+        botaoComecar.textContent = 'Comece Agora'; // Altera o texto para 'Comece Agora'
     } else {
-        // Se não estiver logado, redireciona para a página de cadastro
+        // Se não estiver logado, redireciona para a página de cadastro e muda o texto
         botaoComecar.href = 'cadastro.html';
+        botaoComecar.textContent = 'Cadastre-se Agora'; // Altera o texto para 'Cadastre-se Agora'
     }
 });
